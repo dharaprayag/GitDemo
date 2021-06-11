8f0b19153b63e75526d3c3efee9593fd10bcad26
dharaprayagraj <dharaprayagraj@gmail.com>
2021-06-11 06:16:22 -0400

    message8

--- src/test/java/Academy/HomePage.java
+++ src/test/java/Academy/HomePage.java
@@ -22,10 +22,10 @@ public class HomePage extends Base
 	public void basePageNavigation(String username, String password, String text) throws IOException
 	{
 		try {
-			log.info("This is Info massage7");
-			log.debug("This is Debuug messsage7");
-			log.error("This is error message7");
-			log.fatal("This is fatal message7");
+			log.info("This is Info message8");
+			log.debug("This is Debuug messsage8");
+			log.error("This is error message8");
+			log.fatal("This is fatal message8");
 		driver = initilizeDriver();
 		driver.get(siteUrl);
 		LandingPage l = new LandingPage(driver);
@@ -34,8 +34,8 @@ public class HomePage extends Base
 		l.getLogin().click();
 		lp.getEmail().sendKeys(username);
 		lp.getPassword().sendKeys(password);
-		System.out.println("change to message6");
-		System.out.println("now change to message7");
+		System.out.println("change to message7");
+		System.out.println("now change to message8");
 		
 		log.info(username);
 		lp.getLogin().click();	
